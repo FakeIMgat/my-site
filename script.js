a3e328c4d1c32bb1b7e3b9a48f320482484a6cec
fakeimgat <fakeimgat@gmail.com>
2026-05-26 12:05:26 +0500

    updated links

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("site loaded");
+const card = document.querySelector(".card");
+
+document.addEventListener("mousemove", e => {
+
+  let x = (window.innerWidth / 2 - e.pageX) / 25;
+  let y = (window.innerHeight / 2 - e.pageY) / 25;
+
+  card.style.transform =
+  `rotateY(${x}deg) rotateX(${-y}deg)`;
+});
